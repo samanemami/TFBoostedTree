82b2cf4b68c47814da1c373a91621cadbcea6f05
samanemami <saman.emami@hotmail.com>
2021-08-17 12:56:49 +0200

    update the predict

--- TFBT.py
+++ TFBT.py
@@ -250,4 +250,5 @@ class BoostedTreesRegressor(TFBT):
         score : float or ndarray of floats
         """
         output_errors = np.average((y_true - pred) ** 2, axis=0)
+        
         return np.sqrt(output_errors)
